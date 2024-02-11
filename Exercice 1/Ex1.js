f59865996ec345c12ffd990b0bfce132aab45fe2
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2024-02-11 22:22:14 +0100

    changes in ex1

--- Exercice 1/Ex1.js
+++ Exercice 1/Ex1.js
@@ -1,61 +1,44 @@
-function game(difficulty)
-{
-
-    let min, max, tries;
-    switch(difficulty)
-    {
-        case "easy":
-            min=0;
-            max=15;
-            tries=10;
-            break;
-        case "moderate":
-            min=0;
-            max=30;
-            tries=5;
-            break;
-        case "hard":
-            min=0;
-            max=50;
-            tries=3;
-            break;
-        default:
-            console.error('Invalid difficulty level');
-        return;  
+const setParams = (mn, mx, tr) => {
+  min = mn;
+  max = mx;
+  tries = tr;
+};
+let min, max, tries;
+function game(difficulty) {
+  switch (difficulty) {
+    case "easy":
+      setParams(0, 15, 10);
+      break;
+    case "moderate":
+      setParams(0, 30, 5);
+      break;
+    case "hard":
+      setParams(0, 50, 3);
+      break;
+    default:
+      console.error("Invalid difficulty level");
+      return;
+  }
+  const target = Math.floor(Math.random() * (max - min));
+  for (let i = 0; i < tries; i++) {
+    const guess = parseInt(
+      prompt("Guess the number. It's between " + min + " and " + max)
+    );
+    if (guess === target) {
+      alert("Congrats! You've won");
+      return;
+    } else {
+      alert("Wrong answer. Remaining tries: " + (tries - (i + 1)));
     }
-
- const target=Math.floor(Math.random()*(max-min));
- for(let i=0;i<tries;i++)
- {
-    const guess=parseInt(prompt("Guess the number. It's between " +min+" and "+ max));
-    if (guess===target)
-    {
-        alert("Congrats! You've won");
-        return;
-    }
-    else{
-        alert("Wrong answer. Remaining tries: "+(tries-(i+1)));
-    }
- }
- alert("Sorry, no more tries remaining. The correct answer was "+target);
-
-
+  }
+  alert("Sorry, no more tries remaining. The correct answer was " + target);
 }
 
-const diff=document.querySelector("#difflist");
-diff.addEventListener('click', function(event)
-{
-    if(event.target.id==="easy" || event.target.id==="mod" || event.target.id==="hard")
-    {
-        const difficulty=event.target.getAttribute("value");
-        game(difficulty)
-    }
-
-}
-
-
-)
-
-
-
-
+const diff = document.querySelector("#difflist");
+const menu = document.querySelector(".accordion");
+diff.addEventListener("click", (event) => {
+  if (["easy", "mod", "hard"].includes(event.target.id)) {
+    const difficulty = event.target.getAttribute("value");
+    game(difficulty);
+  }
+});
