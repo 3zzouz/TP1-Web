12d6444df7992e072a91fe724337c1aa060b94c8
LeithEng <147639906+LeithEng@users.noreply.github.com>
2024-02-14 20:29:30 +0100

    Commented Ex1.js

--- Exercice 1/Ex1.js
+++ Exercice 1/Ex1.js
@@ -1,10 +1,13 @@
 let min, max, tries;
+// Function to set the game parameters based on difficulty
 const setParams = (mn, mx, tr) => {
   min = mn;
   max = mx;
   tries = tr;
 };
+// Function to handle the guessing game
 function game(difficulty) {
+  // Set game parameters based on difficulty level
   switch (difficulty) {
     case "easy":
       setParams(0, 15, 10);
@@ -19,8 +22,10 @@ function game(difficulty) {
       console.error("Invalid difficulty level");
       return;
   }
+  
   const target = Math.floor(Math.random() * (max - min));
   for (let i = 0; i < tries; i++) {
+    // Prompt the user for a guess
     const guess = parseInt(
       prompt("Guess the number. It's between " + min + " and " + max)
     );
@@ -31,6 +36,7 @@ function game(difficulty) {
       alert("Wrong answer. Remaining tries: " + (tries - (i + 1)));
     }
   }
+  // Alert the user that no more tries are remaining and reveal the correct answer
   alert("Sorry, no more tries remaining. The correct answer was " + target);
 }
 
@@ -38,6 +44,7 @@ const diff = document.querySelector("#difflist");
 const menu = document.querySelector(".accordion");
 diff.addEventListener("click", (event) => {
   if (["easy", "mod", "hard"].includes(event.target.id)) {
+    //Starts the game after the user chooses the difficulty
     const difficulty = event.target.getAttribute("value");
     game(difficulty);
   }
